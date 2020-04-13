c5e83e2004310c6e8c94f110201edd8b7b367217
댈엄지 <daramkun@live.com>
2020-04-14 00:31:45 +0900

    직렬화 및 역직렬화 메서드를 static 메서드로 변경

---
+++ Blockar/BlockarObject.Csv.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Daramee.Blockar
+{
+	partial class BlockarObject
+	{
+		#region Serialization
+		public void SerializeToCsv (Stream stream)
+		{
+
+		}
+		#endregion
+
+		#region Deserialization
+
+		#endregion
+	}
+}
